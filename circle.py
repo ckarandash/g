e1823933b459b1e1e631359f168563a3ab68671b
mike <mymail@mail.ru>
2024-10-31 10:22:42 +0300

    added docs to files

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''ѕринимает число радиус r, возвращает прощадь круга по формуле pi * r^2'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает радиус r, возвращает периметр круга по формуле 2 * pi * r'''
     return 2 * math.pi * r
 
